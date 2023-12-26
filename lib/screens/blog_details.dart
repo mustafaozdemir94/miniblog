156352656213717ef2322f492813c11da223cddb
Mustafa Özdemir <mustafaozdemiroffical@gmail.com>
2023-12-26 15:03:27 +0300

    bloc_ders_sonu

--- lib/screens/blog_details.dart
+++ lib/screens/blog_details.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+// ignore: unused_import
 import 'dart:io';
 import "package:http/http.dart" as http;
 
@@ -9,6 +10,7 @@ class BlogDetails extends StatefulWidget {
   const BlogDetails({required this.blogId, Key? key}) : super(key: key);
   final String blogId;
   @override
+  // ignore: library_private_types_in_public_api
   _BlogDetailsState createState() => _BlogDetailsState();
 }
 
@@ -18,6 +20,7 @@ class _BlogDetailsState extends State<BlogDetails> {
   fetchBlogs() async {
     Uri url = Uri.parse("https://tobetoapi.halitkalayci.com/api/Articles/${widget.blogId}");
     final response = await http.get(url);
+    // ignore: avoid_print
     print(json.decode(response.body));
     final jsonData = json.decode(response.body);
     setState(() {
@@ -42,6 +45,7 @@ class _BlogDetailsState extends State<BlogDetails> {
       ),
       body: blog.id == null
           ? const Center(child: CircularProgressIndicator())
+          // ignore: avoid_unnecessary_containers
           : Container(
               child: Padding(
                 padding: const EdgeInsets.all(16),
