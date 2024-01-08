603e29143c3f50873b54b7857b48f042f938f999
Mustafa Özdemir <mustafaozdemiroffical@gmail.com>
2024-01-08 22:42:45 +0300

    update

--- lib/screens/add_blog.dart
+++ lib/screens/add_blog.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class AddBlog extends StatefulWidget {
   const AddBlog({Key? key}) : super(key: key);
@@ -19,8 +20,27 @@ class _AddBlogState extends State<AddBlog> {
   String content = "";
   String author = "";
 
-  submit() {
-    // Submit fonksiyonunun kodlanması..
+  submit() async {
+    Uri url = Uri.parse("https://tobetoapi.halitkalayci.com/api/Articles");
+
+    var request = http.MultipartRequest("POST", url);
+
+    request.fields['Title'] = title;
+    request.fields['Content'] = content;
+    request.fields['Author'] = author;
+
+    if (selectedImage != null) {
+      final file = await http.MultipartFile.fromPath("File", selectedImage!.path);
+      request.files.add(file);
+    }
+
+    final response = await request.send();
+
+    if (response.statusCode == 201) {
+      // Ekleme başarılı
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context, true);
+    }
   }
 
   pickImage() async {
@@ -76,7 +96,7 @@ class _AddBlogState extends State<AddBlog> {
                   decoration: const InputDecoration(label: Text("Ad Soyad")),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return "Lütfen ad soyad giriniz";
+                      return "Lütfen ad soyadbaşlığı giriniz";
                     }
                     return null;
                   },
